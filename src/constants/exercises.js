d2db6b527e69ad7fb980ffa016c04b8e06dfabd8
rafael.custodio <rafael.custodio@dextra-sw.com>
2020-01-02 14:12:18 -0300

    .

--- src/constants/exercises.js
+++ src/constants/exercises.js
@@ -1,33 +1,3 @@
-const EXERCISES_TYPE = {
-  CHEST: 'Peito',
-  BACK: 'Costas',
-  LEGS: 'Pernas',
-  BICEPS: 'Biceps',
-  TRICEPS: 'Triceps',
-  ABDOMEN: 'Abdômen',
-  SHOULDER: 'Ombros',
-};
-
-const exercises = {
-  CHEST: [
-    'Supino reto',
-    'Supino inclinado',
-    'Fly',
-    'Fly inclinado',
-    'Peck-Deck',
-  ],
-  BACK: ['Remada alta', 'Remada baixa', 'Barra'],
-  LEGS: [
-    'Agachamento',
-    'Levantamento terra',
-    'Flexora',
-    'Extenssora',
-    'Panturrilha',
-  ],
-  BICEPS: ['Rosca direta', 'Rosca inclinada', 'Rosca unilateral'],
-  TRICEPS: ['Triceps testa', 'Mergulho', 'Triceps Pulley', 'Triceps corda'],
-  SHOULDER: ['Desenvolvimento', 'Elevação lateral'],
-  ABDOMEN: ['Abdominal', 'Abdominal Inclinado'],
-};
+const EXERCISES_TYPE = {};
 
 export { EXERCISES_TYPE };
